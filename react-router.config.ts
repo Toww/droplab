e20726b46ac7de2da3d98ae8a6b7b9e30f0c77d5
Toww <25932452+Toww@users.noreply.github.com>
2025-05-14 13:44:41 +0200

    netlify config

--- react-router.config.ts
+++ react-router.config.ts
@@ -1,11 +1,6 @@
 import type { Config } from "@react-router/dev/config";
-import { vercelPreset } from "@vercel/react-router/vite";
 
 export default {
   appDirectory: "src",
-  ssr: false,
-  presets: [vercelPreset()],
-  async prerender() {
-    return ["/about"];
-  }
+  ssr: true
 } satisfies Config;
--- vite.config.ts
+++ vite.config.ts
@@ -3,10 +3,11 @@ import svgr from "vite-plugin-svgr";
 import glsl from "vite-plugin-glsl";
 import tailwindcss from "@tailwindcss/vite";
 import { reactRouter } from "@react-router/dev/vite";
+import netlifyPlugin from "@netlify/vite-plugin-react-router";
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [reactRouter(), glsl(), tailwindcss(), svgr()],
+  plugins: [reactRouter(), glsl(), tailwindcss(), svgr(), netlifyPlugin()],
   resolve: {
     alias: {
       src: "/src",
